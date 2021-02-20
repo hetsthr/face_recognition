4650307a82926fad3dbb68c44547df963371aa6d
hetsthr <hetsthr@gmail.com>
2021-02-20 15:53:57 +0530

    tflite converter added

--- test.py
+++ test.py
@@ -6,6 +6,8 @@ import os
 import numpy as np
 from os import listdir
 from os.path import isfile, join
+import cv2
+import time
 
 facial_recog_dict = {"[0]" : "Het",
         "[1]" : "Prashant"}
@@ -15,4 +17,17 @@ facial_recog_dict_n = {"n0" : "Het",
 
 def getRandomImage(path):
     """function loads a random image from a random folder in our test path"""
-    
+    img = cv2.imread(path)
+    return img
+
+for i in range(0,50):
+    path = 'face_data/test/'+str(i)+'.jpeg'
+    input_im = getRandomImage(path)
+    input_im = input_im / 255.
+    input_im = input_im.reshape(1,96,96,3)
+    start = time.time()
+    res = np.argmax(classifier.predict(input_im, 1, verbose = 0), axis = 1)
+    stop = time.time()
+    print("Prediction:",facial_recog_dict[str(res)])
+    print("Latency:",stop-start)
+
