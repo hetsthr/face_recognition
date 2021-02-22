e3b53e9aba51b203248d07a0b1e9c26dd177fcbc
hetsthr <hetsthr@gmail.com>
2021-02-23 04:16:01 +0530

    tflite interpreter

--- test_tflite.py
+++ test_tflite.py
@@ -1,3 +1,17 @@
+import numpy as np
 import tensorflow as tf
 
-interpreter = tf.lite.Interpreter(model_content=tflite_model
+interpreter = tf.lite.Interpreter(model_path="MobileNet/mobilenet_50_10/model_quant.tflite")
+interpreter.allocate_tensors()
+
+input_details = interpreter.get_input_details()
+output_details = interpreter.get_output_details()
+
+input_shape = input_details[0]['shape']
+input_data = np.array(np.random.random_sample(input_shape), dtype=np.uint8)
+interpreter.set_tensor(input_details[0]['index'], input_data)
+
+interpreter.invoke()
+
+output_data = interpreter.get_tensor(output_details[0]['index'])
+print(output_data)
