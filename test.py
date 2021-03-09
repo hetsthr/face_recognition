a862c32e79acabcb2ccdf52da086476f77e3b127
hetsthr <hetsthr@gmail.com>
2021-03-10 02:37:47 +0530

    new model

--- test.py
+++ test.py
@@ -1,11 +1,12 @@
-from keras.models import load_model
+import tensorflow as tf
 
-classifier = load_model('MobileNet/mobilenet_25_40_none/Facial_recogNet.h5')
+classifier = tf.keras.models.load_model('MobileNet/mobilenet_25_40_none/Facial_recogNet.h5')
+#classifier = load_model('MobileNet/mobilenet_25_40_none/Facial_recogNet.h5')
 
-import os
+#import os
 import numpy as np
-from os import listdir
-from os.path import isfile, join
+#from os import listdir
+#from os.path import isfile, join
 import cv2
 import time
 
@@ -21,8 +22,10 @@ def getRandomImage(path):
     return img
 correct = 0
 for i in range(0,399):
-    path = 'face_data/test/'+str(i)+'.jpeg'
-    input_im = getRandomImage(path)
+    
+    #path = 'face_data/test/'+str(i)+'.jpeg'
+    #input_im = getRandomImage(path)
+    ret, input_im = cap.read()
     input_im = input_im / 255.
     input_im = input_im.reshape(1,96,96,3)
 
@@ -30,14 +33,10 @@ for i in range(0,399):
     res = np.argmax(classifier.predict(input_im, 1, verbose = 0), axis = 1)
     stop = time.time()
 
-    if ( i <= 199 ):
-        if (facial_recog_dict[str(res)] == "Het"):
-            correct = correct + 1
-    else:
-        if (facial_recog_dict[str(res)] == "Prashant"):
+    if (facial_recog_dict[str(res)] == "Het"):
             correct = correct + 1
 
-    #print("Prediction:",facial_recog_dict[str(res)])
+    print("Prediction:",facial_recog_dict[str(res)])
     print("Latency:",stop-start)
 
 print("Accuracy: ",correct/4)
--- test_tflite.py
+++ test_tflite.py
@@ -1,6 +1,6 @@
 import numpy as np
 import tensorflow as tf
-
+import cv2
 interpreter = tf.lite.Interpreter(model_path="MobileNet/mobilenet_50_10/model_quant.tflite")
 interpreter.allocate_tensors()
 
@@ -8,7 +8,7 @@ input_details = interpreter.get_input_details()
 output_details = interpreter.get_output_details()
 
 input_shape = input_details[0]['shape']
-input_data = np.array(np.random.random_sample(input_shape), dtype=np.uint8)
+input_data = np.array(cv2.imread(), dtype=np.uint8)
 interpreter.set_tensor(input_details[0]['index'], input_data)
 
 interpreter.invoke()
